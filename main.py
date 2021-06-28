c73ae240098ce77135fdd29db7208e2c7e4f673e
Emmanuel Valdez <evaldez89@gmail.com>
2021-06-27 22:26:07 -0400

    [REF] use fastapi and uvicorn

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import asyncio
 from data_scraper.scrape import get_json_response, get_json_response_from_test_file
 from database.mongo_client import MongoClient
+from fastapi import FastAPI
 
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+app = FastAPI()
+mongo_client = MongoClient('localhost', 27017)
 
-    # spendings_items = loop.run_until_complete(get_json_response())
-    spendings_items = loop.run_until_complete(get_json_response_from_test_file())
 
-    mongo_client = MongoClient('localhost', 27017)
-    loop.run_until_complete(mongo_client.save_items(spendings_items))
+async def get_spendings():
+    return await get_json_response_from_test_file()
+
+
+@app.get('/get_entities')
+async def entities():
+    return await mongo_client.get_spending_entities()
+
+
+# if __name__ == '__main__':
+#     loop = asyncio.get_event_loop()
+
+#     # spendings_items = loop.run_until_complete(get_json_response())
+#     spendings_items = loop.run_until_complete(get_json_response_from_test_file())
+
+#     mongo_client = MongoClient('localhost', 27017)
+#     loop.run_until_complete(mongo_client.save_items(spendings_items))
